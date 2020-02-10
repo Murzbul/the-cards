fc976d454d64a0ec976c5d37794520664d872050
nrusso <nrusso@digbang.com>
2020-02-10 17:55:34 -0300

    Add dockerfile code and add partial code from damage effect

--- src/Models/DamageEffect.php
+++ src/Models/DamageEffect.php
@@ -5,18 +5,55 @@ namespace CardsGame\Models;
 use CardsGame\Abstracts\Entity;
 use CardsGame\Contracts\Effect;
 use CardsGame\Traits\EffectTrait;
+use CardsGame\ValueObjects\DamagePoint;
 
 class DamageEffect implements Effect
 {
     use EffectTrait;
 
+    /** @var DamagePoint */
+    private $damagePoint;
+
     public function __construct()
     {
+        $this->damagePoint = new DamagePoint();
     }
 
     public function execute(Entity $executor, ?Entity $player1, ?Entity $player2)
     {
-        // TODO: Implement execute() method.
-        // $entity->appliedDamage();
+        $healthPoints = $player2->getHealth();
+        $shieldPoints = $player2->getShield();
+
+        if ($this->damagePoint->getValue() <= 3 and $shieldPoints > 0) {
+            // Damage: 3 - Health: 15 - Shield: 5 = Damage: 3 - Health: 15 - Shield: 2
+            // Damage: 2 - Health: 15 - Shield: 5 = Damage: 3 - Health: 15 - Shield: 3
+            // Damage: 1 - Health: 15 - Shield: 5 = Damage: 3 - Health: 15 - Shield: 4
+
+            // Damage: 3 - Health: 15 - Shield: 3 = Damage: 3 - Health: 15 - Shield: 0
+            // Damage: 2 - Health: 15 - Shield: 1 = Damage: 3 - Health: 14 - Shield: 0
+            // Damage: 3 - Health: 15 - Shield: 1 = Damage: 3 - Health: 15 - Shield: -2 = Damage: 3 - Health: 13 - Shield: 0
+
+            $shieldPoints -= $this->damagePoint->getValue();
+
+            if ($shieldPoints < 0) {
+                $healthPoints += $shieldPoints;
+                $shieldPoints = 0;
+            }
+        } elseif ($this->damagePoint->getValue() > 3) {
+        }
+
+        // Damage: 8 - Health: 15 - Shield: 5
+
+        dd($this->damagePoint);
+        $this->damagePoint->getValue();
+//        $damage = $this->damagePoint->getValue()
+
+//        $healthPoints = $healthPoints - ;
+
+//        $points += $this->healthPoint->getValue();
+//
+//        $this->healthPoint->setValue($points);
+//
+//        $player1->setHealth($this->healthPoint);
     }
 }
---
+++ src/ValueObjects/DamagePoint.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace CardsGame\ValueObjects;
+
+use CardsGame\Contracts\Attribute;
+
+class DamagePoint implements Attribute
+{
+    /** @var int */
+    private $point;
+
+    public function __construct()
+    {
+        $this->point = random_number(1, 10);
+    }
+
+    public function getValue()
+    {
+        return $this->point;
+    }
+
+    public function setValue(int $point)
+    {
+        $this->point = $point;
+    }
+}
